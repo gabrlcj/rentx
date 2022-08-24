9d95998b308d01a085fa63e8dfa1df9052e95bbd
gabrlcj <gabrielbittencourt57@gmail.com>
2022-08-24 18:25:23 -0300

    Initial commit

---
+++ routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+
+  const category = {
+    id: uuidV4(),
+    name,
+    description,
+  };
+
+  categories.push(category);
+
+  return res.status(201).json(category);
+});
+
+export { categoriesRoutes };
