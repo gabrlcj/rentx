83023f91305737e9ae00385e91b7f2ae373015f4
gabrlcj <gabrielbittencourt57@gmail.com>
2022-08-27 11:45:04 -0300

    Adds singleton for categories repository

--- src/modules/cars/repositories/Categories/CategoriesRepository.ts
+++ src/modules/cars/repositories/Categories/CategoriesRepository.ts
@@ -4,9 +4,18 @@ import { ICategoriesRepository } from './ICategoriesRepository';
 
 export class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
+  private static INSTANCE: CategoriesRepository;
 
-  constructor() {
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
   }
 
   findByName(name: string) {
