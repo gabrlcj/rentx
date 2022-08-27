5f44838ed92ebc77c3c08cbcfaaa7fcbf6649872
gabrlcj <gabrielbittencourt57@gmail.com>
2022-08-27 12:04:50 -0300

    Adds refactor for routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-
-app.use('/specifications', specificationRoutes);
+app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
