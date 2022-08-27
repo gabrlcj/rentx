ad51b2a816328e82a05d87ae9bbf4905ffdf4776
gabrlcj <gabrielbittencourt57@gmail.com>
2022-08-27 11:39:52 -0300

    Adds use case to list all categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/Categories/CategoriesRepository';
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get('/', (req, res) => {
-  const categories = categoriesRepository.getAll();
-
-  return res.json({ categories });
+  return listCategoriesController.handle(req, res);
 });
 
 export { categoriesRoutes };
