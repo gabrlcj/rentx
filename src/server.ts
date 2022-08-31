a2c2074c831169e42f0df05518881e950c5819cc
gabrlcj <gabrielbittencourt57@gmail.com>
2022-08-31 00:31:38 -0300

    Adds swagger for the documentation of the API

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
